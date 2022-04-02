3ec0efd12cd6812b88b99f41c6353db0fb6fb3eb
Sultan Mahmud <kaziiriad@gmail.com>
2022-04-03 03:17:11 +0600

    Project finished

--- script/script.js
+++ script/script.js
@@ -1,38 +1,40 @@
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
+    
+    var year = new Date().getFullYear()+parseInt(years);
 
-    if (principal == "")
-    {
-        alert("Amount cannot be empty!")
+    if (principal == "") {
+        alert("Amount cannot be empty!");
         document.getElementById("principal").focus();
         return false;
-    }
-    else
-    {
-        if (principal <= 0)
-        {
-            alert("Amount cannot be less that zero. Please enter a positive number!");
-            document.getElementById("principal").focus();
-            return false;
-        }
-    }
+  } else {
+    if (principal <= 0) {
+        alert("Amount cannot be less that zero. Please enter a positive number!");
+        document.getElementById("principal").focus();
+        return false;
+    } else {
 
-    document.getElementById("intst").innerText = interest
-    var year = new Date().getFullYear+parseInt(years);
+        var interest = (principal * years * rate) / 100;
+        var result =
+            "If you deposit <mark>" +
+            principal +
+            "</mark>, <br> at an interest rate of <mark>" +
+            rate +
+            "</mark> % <br> You will receive an amount of <mark>" +
+            interest +
+            ", </mark><br> in the year <mark> " +
+            year +
+            "</mark>";
 
-    var result = "If you deposit <mark>"+principal+"</mark>, <br> at an interest rate of <mark>"+rate+"</mark> % <br> You will receive an amount of <mark>"+interest+", </mark><br> in the year <mark> "+year+"</mark>";
-    
-    document.getElementById("res").innerHTML = result;
+        document.getElementById("res").innerHTML = result;
+    }
+  }
+
 }
-        
 
-function updateRate()
-{
+function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText = rateval;
 }
-
