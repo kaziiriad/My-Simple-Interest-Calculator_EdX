f9a5ee53fbd64cc270e285cde371ff204970138e
Sultan Mahmud <kaziiriad@gmail.com>
2022-04-03 03:26:32 +0600

    final commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//Computes interst
 function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
@@ -5,35 +6,38 @@ function compute() {
     
     var year = new Date().getFullYear()+parseInt(years);
 
+    //validates if amount field is empty 
     if (principal == "") {
         alert("Amount cannot be empty!");
         document.getElementById("principal").focus();
         return false;
-  } else {
-    if (principal <= 0) {
-        alert("Amount cannot be less that zero. Please enter a positive number!");
-        document.getElementById("principal").focus();
-        return false;
-    } else {
+    } 
+    else {
+        // validates if amount is less than or equal zero
+        if (principal <= 0) {
+            alert("Amount cannot be less that zero. Please enter a positive number!");
+            document.getElementById("principal").focus();
+            return false;
+        } else {
 
-        var interest = (principal * years * rate) / 100;
-        var result =
-            "If you deposit <mark>" +
-            principal +
-            "</mark>, <br> at an interest rate of <mark>" +
-            rate +
-            "</mark> % <br> You will receive an amount of <mark>" +
-            interest +
-            ", </mark><br> in the year <mark> " +
-            year +
-            "</mark>";
+            var interest = (principal * years * rate) / 100;
+            var result =
+                "If you deposit <mark>" +
+                principal +
+                "</mark>, <br> at an interest rate of <mark>" +
+                rate +
+                "</mark> % <br> You will receive an amount of <mark>" +
+                interest +
+                ", </mark><br> in the year <mark> " +
+                year +
+                "</mark>";
 
-        document.getElementById("res").innerHTML = result;
+            document.getElementById("res").innerHTML = result;
+        }
     }
-  }
 
 }
-
+// updates rate slider
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
